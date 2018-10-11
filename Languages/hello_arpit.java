2c73a97818229f57651b68d4273367aae10bd4a1
Arpit Goyal <arpitgoyal437@gmail.com>
2018-10-11 18:52:31 +0530

    Hello program, Happy Hacktopberfest!!

---
+++ Languages/hello_arpit.java
@@ -0,0 +1,15 @@
+class Hello 
+{
+    public void get_hello()
+    {
+        System.out.println("Hello World !!!");
+    } //end get_hello method
+    
+    public static void main(String args[])
+    {
+        Hello ch = new Hello();
+        ch.get_hello();
+        System.out.println("Done");
+    } //end main
+} // class Hello
+     
